82624dc5f95e1552c82adb9a1386bcdb153bde3c
moateff <moatef.yousef@gmail.com>
2025-03-24 01:09:17 +0200

    Commit last

--- MIPS-Register-File/REG_FILE.vhd
+++ MIPS-Register-File/REG_FILE.vhd
@@ -23,16 +23,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL; 
 
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity REG_FILE is
     Port ( 
         clk       : in  STD_LOGIC;                      -- Clock signal
